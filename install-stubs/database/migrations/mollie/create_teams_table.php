291dfe04be68f9c57cd5af684866050f9f57adcc
Sander van Hooft <info@sandervanhooft.nl>
2019-06-14 13:31:42 +0200

    Mollie specific migrations

---
+++ install-stubs/database/migrations/mollie/create_teams_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teams', function (Blueprint $table) {
+
+            // Generic Spark
+            $table->increments('id');
+            $table->unsignedInteger('owner_id')->index();
+            $table->string('name');
+            $table->string('slug')->nullable()->unique();
+            $table->text('photo_url')->nullable();
+
+            // Cashier Mollie
+            $table->string('mollie_customer_id')->nullable();
+            $table->string('mollie_mandate_id')->nullable();
+            $table->text('extra_billing_information')->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
+
+            // Generic Spark
+            $table->string('current_billing_plan')->nullable();
+
+            // Not sure if required, stored on Mollie mandate
+            // $table->string('card_brand')->nullable();
+            // $table->string('card_last_four')->nullable();
+
+            // Generic Spark
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teams');
+    }
+}
