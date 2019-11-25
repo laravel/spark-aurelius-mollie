a8404e1f366892f4a135ea70f2ba5f802d5cbd9f
Sander van Hooft <info@sandervanhooft.nl>
2019-11-25 14:12:08 +0100

    Revert to using local invoices table

--- src/Repositories/MolliePerformanceIndicatorsRepository.php
+++ src/Repositories/MolliePerformanceIndicatorsRepository.php
@@ -2,46 +2,8 @@
 
 namespace Laravel\Spark\Repositories;
 
-use Laravel\Spark\Spark;
-use Illuminate\Support\Facades\DB;
-use Laravel\Spark\Team;
-use Mollie\Api\Types\PaymentStatus;
-
 class MolliePerformanceIndicatorsRepository extends PerformanceIndicatorsRepository
 {
-
-    /**
-     * {@inheritdoc}
-     */
-    public function totalRevenueForUser($user)
-    {
-        $total = DB::table('orders')
-            ->where('mollie_payment_status', PaymentStatus::STATUS_PAID)
-            ->where('owner_id', $user->id)
-            ->where('owner_type', get_class($user))
-            ->sum('total');
-
-        if(Spark::usesTeams()) {
-            $total += DB::table('orders')
-                ->where('mollie_payment_status', PaymentStatus::STATUS_PAID)
-                ->where('owner_type', Team::class)
-                ->whereIn('team_id', $user->teams->pluck('id')->all())
-                ->sum('total');
-        }
-
-        return $total;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function totalVolume()
-    {
-        return DB::table('orders')
-            ->where('mollie_payment_status', PaymentStatus::STATUS_PAID)
-            ->sum('total') / 100;
-    }
-
     /**
      * Get the plan column name.
      *
